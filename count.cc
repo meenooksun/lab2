603a32b6d60f25f4dbaa8b5dda7882520e41429c
meenooksun <meenook1109@gmail.com>
2022-09-05 20:27:56 +0900

    add count.cc

---
+++ count.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "count.h"
+
+using namespace std;
+
+int count_(int num){
+
+	int i;
+	for( i=1; i<=num;i++){
+		cout << i << endl;
+	}
+	cout << "Start!!" << endl;
+	cout << "[2017310318]" << "" << "[meenooksun]" <<  endl;
+}
